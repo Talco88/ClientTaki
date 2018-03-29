c28d5768f0a2651ede2621b28d5b97fdbcff6455
Talco88 <talcohen88@gmail.com>
2018-03-29 19:25:46 +0300

    create trash Deck

--- Taki/JS/BoardLogic.js
+++ Taki/JS/BoardLogic.js
@@ -1,23 +1,16 @@
 ﻿var boardLogic = {};
-boardLogic.cardColors = Object.freeze({"Green":0, "Red":1, "Yellow":2, "Blue":3, "Non":4});
-boardLogic.cardOptions = Object.freeze({"1":1, "+2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "Taki":10, "Stop":11, "ChangeColor":12, "+":13})
+boardLogic.cardColors = Object.freeze({ 0: "Green", 1: "Red", 2: "Yellow", 3: "Blue", 4: "Non" });
+boardLogic.cardOptions = Object.freeze({ 1: "1", 2: "+2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "Taki", 11: "Stop", 12: "ChangeColor", 13: "+" })
 
-
-boardLogic.testUI = function () {
-    var testDiv = document.getElementById("cards");
-    for (var i = 0; i < boardLogic.deckOfCards.length; i++) {
-        testDiv.innerHTML += "</br><li>" + boardLogic.deckOfCards[i].number + " " + boardLogic.deckOfCards[i].color + "</li>";
-    }
-}
 
 boardLogic.init = function () {
     boardLogic.deckOfCards = [];
+    boardLogic.trashDeck = [];
     for (var i = 1; i <= Object.keys(boardLogic.cardOptions).length; i++) {
         if (i != 2) {
             for (var j = 0; j < (Object.keys(boardLogic.cardColors).length - 1); j++) {
                 boardLogic.addGeneratedCards(i, j);
             }
-            
         }
     }
 
@@ -26,7 +19,7 @@ boardLogic.init = function () {
 
 boardLogic.addGeneratedCards = function (number, color) {
     if (number == 12) {
-        boardLogic.deckOfCards.push(boardLogic.createCard(number, color));
+        boardLogic.deckOfCards.push(boardLogic.createCard(number, 4));
     }
     else {
         boardLogic.deckOfCards.push(boardLogic.createCard(number, color));
@@ -52,6 +45,33 @@ boardLogic.shuffleDeck = function (iDeck) {
     }
 }
 
+boardLogic.GetCardFromDeck = function () {
+    if (boardLogic.deckOfCards.length == 0) {
+        boardLogic.deckOfCards = boardLogic.trashDeck;
+        boardLogic.shuffleDeck(boardLogic.deckOfCards);
+        boardLogic.trashDeck = [];
+    }
+    return boardLogic.deckOfCards.shift();
+}
+
+boardLogic.trowCardToTrash = function (iCard) {
+    boardLogic.trashDeck.push(iCard);
+}
+
+boardLogic.getCardFromPlayer = function (iCard) {
+    if (boardLogic.cardRooles(iCard)) {
+        boardLogic.trowCardToTrash(boardLogic.currentCard);
+        boardLogic.currentCard = iCard;
+        return true;
+    }
+
+    return false;
+}
+
+boardLogic.cardRooles = function (iCard){
+    // turn logic here, also mark the flags like +2 or the stop or + etc...
+    return (iCard.color === boardLogic.currentCard.color || iCard.number === boardLogic.currentCard.number);
+}
 
 // UI manipulation section
 boardLogic.loadUI = function () {
@@ -66,8 +86,32 @@ boardLogic.loadUI = function () {
     // test display all the cards on the page
     ////////
     boardLogic.testUI();
+    var startBtn = document.getElementById("drowCard");
+    if (startBtn) {
+        startBtn.onclick = testDrowingCard;
+    }
+}
+
+
+boardLogic.testUI = function () {
+    var testDiv = document.getElementById("cards");
+    testDiv.innerHTML = "";
+    for (var i = 0; i < boardLogic.deckOfCards.length; i++) {
+        testDiv.innerHTML += "</br><li>" + boardLogic.cardOptions[boardLogic.deckOfCards[i].number] + " " + boardLogic.cardColors[boardLogic.deckOfCards[i].color] + "</li>";
+    }
 }
 
 boardLogic.removeFromView = function () {
     boardLogic.divInDOM.style.display = utility.displayHidden;
+}
+
+
+function testDrowingCard() {
+    var currentCard = document.getElementById("currentCard");
+    if (boardLogic.currentCard != undefined) {
+        boardLogic.trowCardToTrash(boardLogic.currentCard);
+    }
+    boardLogic.currentCard = boardLogic.GetCardFromDeck();
+    currentCard.innerHTML = "Nmber: " + boardLogic.cardOptions[boardLogic.currentCard.number] + " Color: " + boardLogic.cardColors[boardLogic.currentCard.color];
+    boardLogic.testUI();
 }
